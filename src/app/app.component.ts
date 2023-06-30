9d4f795fcab235d4f25cd7d9613010cadb58f35f
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2023-06-30 16:37:21 -0500

    Change prefix to ng-social

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ngsocial-root',
+  selector: 'ng-social-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
