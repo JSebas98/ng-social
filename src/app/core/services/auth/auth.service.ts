babf12a5732dbf246438395558a22e3cb1ae90d4
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2023-07-13 01:18:35 -0500

    Add logic to store and manipulate token

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User, UserDTO } from '../../models/user.models';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 interface UserToken {
   access_token: string;
@@ -11,19 +12,44 @@ interface UserToken {
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly AUTH_TOKEN = 'ng-social-super-secret-token-2023';
+  private readonly AUTH_TOKEN = 'ng-social-auth-token';
   private readonly BASE_URL = 'http://localhost:3000/auth';
 
   constructor(
+    @Inject(PLATFORM_ID)
+    private platformId: Object,
     private http: HttpClient
   ) { }
 
+  get token(): string | null {
+    const isBrowser: boolean = isPlatformBrowser(this.platformId);
+    if (isBrowser) {
+      return localStorage.getItem(this.AUTH_TOKEN);
+    }
+
+    return null;
+  }
+
   isLoggedIn() {
-    return true;
+    const isBrowser: boolean = isPlatformBrowser(this.platformId);
+    if (isBrowser) {
+      return !!this.token
+    }
+
+    return false;
   }
 
   login(userData: UserDTO): Observable<UserToken> {
-    return this.http.post<UserToken>(`${this.BASE_URL}/login`, userData);
+    return this.http
+      .post<UserToken>(`${this.BASE_URL}/login`, userData)
+      .pipe(tap(userToken => this.saveUserToken(userToken.access_token)));
+  }
+
+  private saveUserToken(userToken: string) {
+    const isBrowser: boolean = isPlatformBrowser(this.platformId);
+    if (isBrowser) {
+      localStorage.setItem('ng-social-auth-token', userToken);
+    }
   }
 
   signUp(userData: UserDTO) {
